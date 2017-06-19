c25420f0782fc6f3de707290c1e77fc321d7f841
yStasiv <Yurastasiv@hotmail.com>
2017-06-19 14:42:39 +0300

    Домашнє завдання

---
+++ main.js
@@ -0,0 +1,21 @@
+	//var userLog = document.getElementById("Log").value;
+	//var userPass = document.getElementById("Pass");?????????????
+
+function TestAll() {
+	if(document.getElementById("Log").value == "admin"){
+		if(document.getElementById("Pass").value == '123'){
+			alert('Welcome!');
+			// alert('You know me?');
+			// alert('Add me to friends');
+			// alert('OK');
+			location.href = "https://www.ukrainians.co/id69753";
+		}else alert('Password is\'t correct, try again');
+	}else alert('Login is\'t correct, try again');
+}
+
+function Help(){
+	var helper = prompt('12^2 = ?', 'Your Answer');
+	if(helper == 144) alert('Password from 1 til ...');
+	else if(helper == null) alert('are you really admin?');
+	else alert('Learn math, baby...');
+}
